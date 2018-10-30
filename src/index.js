0487c2cf49d330cc6c8f1e57146ae7b9a64ebaa9
zhoubinhai <1987887482@qq.com>
2018-10-30 20:53:08 +0800

    dev分支提交

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {Button} from 'antd-mobile'
 
 ReactDOM.render(
-    <Button type='primary'>登录</Button>
+  (<Button type='primary'>登录</Button>)
   , document.getElementById('root'));
 
 
