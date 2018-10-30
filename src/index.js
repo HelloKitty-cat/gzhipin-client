e87ca599e44dce7c9a611801623b89940bce390e
zhoubinhai <1987887482@qq.com>
2018-10-30 20:47:39 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Button} from 'antd-mobile'
+
+ReactDOM.render(
+    <Button type='primary'>登录</Button>
+  , document.getElementById('root'));
+
+
